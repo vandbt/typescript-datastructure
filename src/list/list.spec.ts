f86c0a77a5b7fc8de55a37570b58fc9ff4c54d95
vandbt <sharkspin@gmail.com>
2018-05-02 23:16:08 +0900

    feat(List<T>) : implements add method and write test specs  (#4)

--- src/list/list.spec.ts
+++ src/list/list.spec.ts
@@ -13,5 +13,36 @@ describe('List<T>', () => {
         });
     });
 
+    describe('add function', () => {
+
+        var list;
+        before(() => {
+            list = new List<Number>();
+        });
+
+        it('should return 0 when 0 added at index 0', () => {
+            list.add(0, 0);
+
+            expect(list.get(0)).to.equal(0);
+        });
+
+        it('should return 1 when 1 added at index 1', () => {
+            list.add(1, 1);
+
+            expect(list.get(1)).to.equal(1);
+        });
+
+        it('should return 10 when 10 added at index 10', () => {
+            list.add(10, 10);
+            expect(list.get(10)).to.equal(10);
+        });
+
+        it('should return 10 when 20 added at index 10 and get(11)', () => {
+            list.add(10, 20);
+            // expect(list.get(10)).to.equal(20);
+            expect(list.get(11)).to.equal(10);
+        });
+    });
+
 
 });
--- src/list/list.ts
+++ src/list/list.ts
@@ -1,13 +1,99 @@
+// ADT from http://www.doc.ic.ac.uk/~ar3/lectures/ProgrammingII/LargePrintOut/Lecture2PrintOut.pdf
+
 class List<T> {
     constructor() {
 
     }
 
-    add(T): T {
-        throw "Not Implemented";
-    };
+    items: Array<T> = [];
+    // length:Number = -1;
 
-    getName(): String {
+    /**
+     * operation to add data to a List
+     * 
+     * @param {Number} index 
+     * @param {any} T 
+     * @returns {T} 
+     * @memberof List
+     */
+    add(index: number, item:T): void {
+
+        if (index < 0) {
+            //TODO: change to global Error definitions.
+            throw "Out of Bound.";
+        }
+
+        if (index > this.items.length) {
+            var delta: number = 0;
+            delta = (index - this.items.length);
+            while (delta--) {
+                this.items.push(null);
+            }
+            this.items[index] = item;
+
+        } else {
+            // extend list
+            // shift items to next
+
+            // save last
+            var last:T = this.items[this.items.length -1];
+            this.items.push(null);
+            const length = this.items.length;
+
+            for (var position:number = index; position < length; position++) {
+                var next:T = this.items[position];
+                this.items[position+1] = next;
+            }
+            this.items[index] = item;
+            this.items[this.items.length -1] = last;
+        }
+    }
+
+    /**
+     * operation to delete a data item from a List
+     * 
+     * @param {any} index 
+     * @returns {T} 
+     * @memberof List
+     */
+    remove(index): T {
+        throw "Not Implemented";
+    }
+
+    /**
+     * to test whether the List is empty
+     * 
+     * @returns {T} 
+     * @memberof List
+     */
+    isEmpty(): Boolean {
+        throw "Not Implemented";
+    }
+
+    /**
+     * t to return one item at a particular position. 
+     * 
+     * @param {any} index 
+     * @returns {T} 
+     * @memberof List
+     */
+    get(index:number): T {
+        //TODO: implements bound
+        return this.items[index];
+    }
+
+    size(): number {
+        throw "Not Implemented";
+    }
+
+    print(): void {
+        for (var index:number = 0; index < this.items.length; index++) {
+            const element = this.items[index];
+            global.console.log('index %d, value %s', index, element);
+        }
+    }
+
+    getName(): string {
         return 'List<T>';
     };
 };
